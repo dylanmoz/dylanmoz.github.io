af2b5ce28542da74c7e75e2af40787dbc7c6a9fe
Dylan Mozlowski <dmozlowski@classy.org>
2017-09-04 18:39:57 -0700

    Add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,14 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
   },
-  plugins: [`gatsby-plugin-react-helmet`, 'gatsby-plugin-glamor'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-glamor',
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-105848707-1',
+      },
+    }
+  ],
 }
